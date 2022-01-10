3909829f6a48e1849daff240fc65d77d048611be
SU-Ki-MA <jsivabalan2002@gmail.com>
2022-01-10 20:19:15 +0530

    initial commit

---
+++ basic_programs/1_add2no.py
@@ -0,0 +1,4 @@
+a=float(input("Enter num1 : "))
+b=float(input("Enter num2 : "))
+sum1=a+b
+print("The sum of {0} and {1} is {2}".format(a,b,sum1))
---
+++ basic_programs/2_max2no.py
@@ -0,0 +1,8 @@
+def maximum(a,b):
+    if(a>=b):
+        return a
+    elif(b>a):
+        return b 
+num1=float(input("Enter num1 :"))
+num2=float(input("Enter num2 :"))
+print(maximum(num1,num2))
